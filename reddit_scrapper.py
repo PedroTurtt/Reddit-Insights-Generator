cc3a0af05fc10955b2836d94f2f464b46b891540
Pedro Turtt <pedroleonardoturtt@gmail.com>
2025-11-03 17:34:54 -0300

    Adding Credencials to test Actions

--- reddit_scrapper.py
+++ reddit_scrapper.py
@@ -1,3 +1,10 @@
 import  praw
 
-print("First commit (Just for test)")
+reddit = praw.Reddit(
+    client_id="",
+    client_secret="",
+    user_agent=""
+)
+
+subreddit_choice = input("Qual Subreddit deseja pesquisar?: ")
+subreddit = reddit.subreddit(subreddit_choice)
