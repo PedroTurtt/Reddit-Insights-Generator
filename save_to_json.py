99416ea147d72dc13c98cd94e5392e29bcf7596a
Pedro Turtt <pedroleonardoturtt@gmail.com>
2025-11-04 00:54:22 -0300

    Add function to load jsons

--- save_to_json.py
+++ save_to_json.py
@@ -8,3 +8,15 @@ def save_json(filename, info):
 
     except Exception as e:
         print(f"Erro ao gerar Json: {e}")
+
+def load_json(file_name_to_load):
+    try:
+        with open(file_name_to_load, "r", encoding="utf-8") as f:
+            dados = json.load(f)
+            print(f"Dados carregados com sucesso!")
+    except FileNotFoundError:
+        print(f"Erro, arquivo não foi encontrado")
+        return None
+    except Exception as e:
+        print(f"Erro ao ler arquivo: {e}")
+        return None
