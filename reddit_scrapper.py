9d29739de2145a788faa3b3bc454560ba264014e
Pedro Turtt <pedroleonardoturtt@gmail.com>
2025-11-04 13:20:52 -0300

    Changing this file to a big function and adding some options to choose, Subreddit,  what to search on the Subreddit, number of posts to save and type of the search. Need some visual upgrades

--- reddit_scrapper.py
+++ reddit_scrapper.py
@@ -1,7 +1,20 @@
-import  praw
+import praw
 import os
 from dotenv import load_dotenv
-from save_to_json import save_json
+from json_manager import save_json
+
+def case_match(value):
+    match value:
+        case 1:
+            return "relevance" 
+        case 2:
+            return "hot"
+        case 3:
+            return "top"
+        case 4:
+            return "new"
+        case _:
+            return "new"
 
 load_dotenv()
 
@@ -12,44 +25,59 @@ reddit = praw.Reddit(
     username=os.environ.get("REDDIT_USERNAME"),
     password=os.environ.get("REDDIT_PASSWORD")
 )
+def reddit_parameters():
+    subreddit_choice = input("Digite o nome do Subreddit desejado: ")
+    subreddit = reddit.subreddit(subreddit_choice)
+    busca = input("O que deseja procurar?: ")
+    limit_number = int(input("Quantidade de posts para salvar: "))
+    
+    print(f"1-Relevance|2-Hot|3-Top|4-New")
+    value = input()
+    try:
+        value_int = int(value)
+    except ValueError:
+        print(f"Entrada inválida '{value}'. Usando a opção padrão.")
+        value = 1
 
-subreddit_choice = input("Digite o nome do Subreddit desejado: ")
-subreddit = reddit.subreddit(subreddit_choice)
-busca = input("O que deseja procurar?: ")
+    search = case_match(value)
+    print("pesquisa escolhida: ",search)
 
-collected_info = []
-print('Iniciando a busca: ')
+    print('subreddit escolhido: ',subreddit)
+    print('termo para busca: ',busca)
+
+    collected_info = []
+    print('Iniciando a busca: ')
 
 
-for submission in subreddit.search(busca, sort="relevance", limit=3):
+    for submission in subreddit.search(busca, sort=search, limit=limit_number):
 
-    post_info = {
-        "id": submission.id,
-        "titulo": submission.title,
-        "autor": str(submission.author),
-        "texto_post": submission.selftext,
-        "comentarios": []
-    }
-
-    print(f"coletando post: {submission.title}")
-
-    submission.comments.replace_more(limit=0)
-  
-    for comment in submission.comments.list():
-        comnetario_info = {
-            "autor": str(comment.author),
-            "comentario": comment.body,
-            "respostas": []
+        post_info = {
+            "id": submission.id,
+            "titulo": submission.title,
+            "autor": str(submission.author),
+            "texto_post": submission.selftext,
+            "comentarios": []
         }
-        for reply in comment.replies:
-            resposta_info = {
-            "autor": str(reply.author),
-            "texto": reply.body
+
+        print(f"coletando post: {submission.title}")
+
+        submission.comments.replace_more(limit=0)
+    
+        for comment in submission.comments.list():
+            comnetario_info = {
+                "autor": str(comment.author),
+                "comentario": comment.body,
+                "respostas": []
             }
-            comnetario_info["respostas"].append(resposta_info)
+            for reply in comment.replies:
+                resposta_info = {
+                "autor": str(reply.author),
+                "texto": reply.body
+                }
+                comnetario_info["respostas"].append(resposta_info)
 
-        post_info["comentarios"].append(comnetario_info)
+            post_info["comentarios"].append(comnetario_info)
 
-    collected_info.append(post_info)
+        collected_info.append(post_info)
 
-save_json("dados_reddit.json", collected_info)
+    save_json("dados_reddit.json", collected_info)
